bf5e0bf23f2855cbea8dd4f80d4b1b45c8bc1012
ryan-dozier <ryanmdozier@gmail.com>
2024-08-27 21:08:03 -0400

    fixing imports

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,9 +1,11 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import * as express from 'express'
+import {Request, Response} from 'express'
 
-export default (req: { params: string }, res : { send: (s: string) => void }) => {
-  const num = req.params;
+export default (req: Request, res : Response) => {
+  const { num } = req.params;
 
   const fibN: number = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
